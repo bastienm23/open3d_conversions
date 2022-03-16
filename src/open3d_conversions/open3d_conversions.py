2b821dacb854797da9c6d88c1710586add706d6b
Maxpad <maxime.padrin@sigma-clermont.fr>
2022-03-16 11:46:41 +0100

    add rgba handling
    this will convert rgba point cloud to rgb one

--- src/open3d_conversions/open3d_conversions.py
+++ src/open3d_conversions/open3d_conversions.py
@@ -51,6 +51,41 @@ def to_msg(open3d_cloud, frame_id=None, stamp=None):
     return pc2.create_cloud(header, fields, cloud_data)
 
 
+def split_rgba_field(cloud_arr):
+    '''Takes an array with a named 'rgb' float32 field, and returns an array in which
+    this has been split into 3 uint 8 fields: 'r', 'g', and 'b'.
+ 
+    (pcl stores rgb in packed 32 bit floats)
+    '''
+    rgb_arr = cloud_arr['rgba'].copy()
+    rgb_arr.dtype = np.uint32
+    r = np.asarray((rgb_arr >> 16) & 255, dtype=np.uint8)
+    g = np.asarray((rgb_arr >> 8) & 255, dtype=np.uint8)
+    b = np.asarray(rgb_arr & 255, dtype=np.uint8)
+     
+    # create a new array, without rgb, but with r, g, and b fields
+    new_dtype = []
+    for field_name in cloud_arr.dtype.names:
+        field_type, field_offset = cloud_arr.dtype.fields[field_name]
+        if not field_name == 'rgb':
+            new_dtype.append((field_name, field_type))
+    new_dtype.append(('r', np.uint8))
+    new_dtype.append(('g', np.uint8))
+    new_dtype.append(('b', np.uint8))    
+    new_cloud_arr = np.zeros(cloud_arr.shape, new_dtype)
+    
+    # fill in the new array
+    for field_name in new_cloud_arr.dtype.names:
+        if field_name == 'r':
+            new_cloud_arr[field_name] = r
+        elif field_name == 'g':
+            new_cloud_arr[field_name] = g
+        elif field_name == 'b':
+            new_cloud_arr[field_name] = b
+        else:
+            new_cloud_arr[field_name] = cloud_arr[field_name]
+    return new_cloud_arr
+
 def from_msg(ros_cloud):
     xyzrgb_array = ros_numpy.point_cloud2.pointcloud2_to_array(ros_cloud)
 
@@ -64,9 +99,20 @@ def from_msg(ros_cloud):
     points[..., 1] = cloud_array['y']
     points[..., 2] = cloud_array['z']
     open3d_cloud.points = open3d.utility.Vector3dVector(points)
-
+    
     if 'rgb' in xyzrgb_array.dtype.names:
         rgb_array = ros_numpy.point_cloud2.split_rgb_field(xyzrgb_array)
+        cloud_array = rgb_array[mask]
+
+        colors = np.zeros(cloud_array.shape + (3,), dtype=np.float)
+        colors[..., 0] = cloud_array['r']
+        colors[..., 1] = cloud_array['g']
+        colors[..., 2] = cloud_array['b']
+
+        open3d_cloud.colors = open3d.utility.Vector3dVector(colors / 255.0)
+        
+    if 'rgba' in xyzrgb_array.dtype.names:
+        rgb_array = split_rgba_field(xyzrgb_array)
         cloud_array = rgb_array[mask]
 
         colors = np.zeros(cloud_array.shape + (3,), dtype=np.float)
